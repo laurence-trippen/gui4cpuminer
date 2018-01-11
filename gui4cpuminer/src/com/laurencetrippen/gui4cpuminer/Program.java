b6b5c94711f15962debf0b9ff5bdb6a5e50c7f01
Laurence Trippen <laurence.trippen@gmx.de>
2018-01-11 19:16:23 +0100

    initial commit

---
+++ gui4cpuminer/src/com/laurencetrippen/gui4cpuminer/Program.java
@@ -0,0 +1,16 @@
+package com.laurencetrippen.gui4cpuminer;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Program extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+			
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
