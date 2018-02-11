c7e540ac13861d25962bed36c77df46c8326daf6
laurence <laurence@192.168.100.64>
2018-02-11 01:20:01 +0100

    textfield styled

--- gui4cpuminer/src/com/laurencetrippen/gui4cpuminer/view/scene/DashboardScene.java
+++ gui4cpuminer/src/com/laurencetrippen/gui4cpuminer/view/scene/DashboardScene.java
@@ -8,6 +8,7 @@ import javafx.event.ActionEvent;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 
@@ -19,6 +20,7 @@ public class DashboardScene extends AbstractScene {
 	private Button startButton;
 	private Button dashboardButton;
 	private Button configButton;
+	private TextField textField;
 	
 	public DashboardScene(MainStage mainStage) {
 		super(new AnchorPane(), 1024, 768);
@@ -30,9 +32,10 @@ public class DashboardScene extends AbstractScene {
 	@Override
 	protected void initScene() {
 		this.root = (AnchorPane) this.getRoot();
-		this.startButton = new Button("Start");
+		this.startButton = new Button("Start Mining");
 		this.dashboardButton = new Button("Dashboard");
 		this.configButton = new Button("Configuration");
+		this.textField = new TextField();
 		this.sidePane = new Pane();
 	}
 
@@ -59,13 +62,23 @@ public class DashboardScene extends AbstractScene {
 		sidePaneChilds.add(dashboardButton);
 		sidePaneChilds.add(configButton);
 		
+		this.startButton.setPrefWidth(200);
+		this.startButton.setPrefHeight(40);
 		this.startButton.setLayoutX(300);
-		this.startButton.setLayoutY(100);
+		this.startButton.setLayoutY(70);
 		this.startButton.setOnAction(this::onStartButtonClick);
+		this.startButton.getStyleClass().add("ui-button");
+		
+		this.textField.setLayoutX(300);
+		this.textField.setLayoutY(120);
+		this.textField.setPrefSize(256, 40);
+		this.textField.setPromptText("Type in here");
+		this.textField.getStyleClass().add("ui-textfield");
 		
 		ObservableList<Node> rootNodes = root.getChildren();
 		rootNodes.add(startButton);
 		rootNodes.add(sidePane);
+		rootNodes.add(textField);
 		
 		AnchorPane.setBottomAnchor(sidePane, 0.0);
 		AnchorPane.setLeftAnchor(sidePane, 0.0);
