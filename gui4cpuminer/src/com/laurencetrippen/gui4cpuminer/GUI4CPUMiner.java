015ebbd6af5a1360f5a532a62a1da27704ae6572
laurence <laurence@192.168.100.64>
2018-02-17 14:35:13 +0100

    deleted unpractical stage class

--- gui4cpuminer/src/com/laurencetrippen/gui4cpuminer/GUI4CPUMiner.java
+++ gui4cpuminer/src/com/laurencetrippen/gui4cpuminer/GUI4CPUMiner.java
@@ -1,23 +1,27 @@
 package com.laurencetrippen.gui4cpuminer;
 
-import com.laurencetrippen.gui4cpuminer.view.stage.MainStage;
+import com.laurencetrippen.gui4cpuminer.view.scene.MainScene;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class GUI4CPUMiner extends Application {
 
+	private MainScene mainScene;
+	
 	@Override
 	public void init() throws Exception {
-		
+		this.mainScene = new MainScene();
 	}
 	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		primaryStage = null;
-		
-		MainStage mainStage = new MainStage("gui4cpuminer");
-		mainStage.show();
+		primaryStage.setTitle("gui4cpuminer");
+		primaryStage.setResizable(false);
+		primaryStage.setMaxWidth(1024);
+		primaryStage.setMaxHeight(768);
+		primaryStage.setScene(mainScene);
+		primaryStage.show();
 	}
 	
 	public static void main(String[] args) {
